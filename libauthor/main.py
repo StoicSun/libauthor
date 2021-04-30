808e5332965eb1a937dffd17dc1fa1240884a9f8
Suraj <65294601+StoicSun@users.noreply.github.com>
2021-04-30 07:07:04 +0000

    fixed tabs and spaces inconsistency

--- libauthor/main.py
+++ libauthor/main.py
@@ -22,14 +22,15 @@ def searcher(s, author, match="F", lang="English", ext="pdf"):
 
 
 def downloader(titles):
-		s = LibgenSearch()
-		download_links = s.resolve_download_links(titles)
-		title = titles['Title']
-		with console.status("[i deep_pink2]Sneaking into the library[/i deep_pink2]\n", spinner="aesthetic", spinner_style='blue_violet',speed=0.5):
-			r = requests.get(download_links['IPFS.io'])
-		with open(f'{title}.pdf', 'wb') as f:
-			f.write(r.content)		
-		console.print(f"[dark_turquoise]Stole {title} for you[/dark_turquoise] 🦝")
+    s = LibgenSearch()
+    download_links = s.resolve_download_links(titles)
+    title = titles['Title']
+    with console.status("[i deep_pink2]Sneaking into the library[/i deep_pink2]\n", spinner="aesthetic", spinner_style='blue_violet',speed=0.5):
+        r = requests.get(download_links['IPFS.io'])
+    with open(f'{title}.pdf', 'wb') as f:
+        f.write(r.content)		
+    console.print(f"[dark_turquoise]Stole {title} for you[/dark_turquoise] 🦝")
+
 
 s = LibgenSearch()  # instantiate the Search object
 console.print(md)
@@ -38,20 +39,19 @@ author.capitalize
 choice = input(
     "Do you want to change the search criteria? Type Y/N, (Default is N): ")
 if choice == "Y":
-		console.rule("[bold red]Config", style='dark_orange')
-		lang = input("Which language do you want those books in?, (Default is English): ")
-		lang.capitalize
-		ext = input(
-        "What format[pdf,epub,djvu,cbz,etc.] do you want the books in?, (Default is pdf): ")
-		ext.lower
-		match = input("Would like an exact match? Type Y/N, (Default is N): ")
-		t = searcher(s, author, match, lang, ext)
+    console.rule("[bold red]Config", style='dark_orange')
+    lang = input("Which language do you want those books in?, (Default is English): ")
+    lang.capitalize
+    ext = input("What format[pdf,epub,djvu,cbz,etc.] do you want the books in?, (Default is pdf): ")
+    ext.lower
+    match = input("Would like an exact match? Type Y/N, (Default is N): ")
+    t = searcher(s, author, match, lang, ext)
 else:
     t = searcher(s, author)
 path = f'{os.getcwd()}' + f"/{t[0]['Author']}"
 os.mkdir(path)
 os.chdir(path)		
 with concurrent.futures.ProcessPoolExecutor() as executor:
-        for books in t:
-            executor.submit(downloader, books)
+    for books in t:
+        executor.submit(downloader, books)
 console.print("[b chartreuse2]Got the books [/b chartreuse2]✔️", justify="left")						
